9b542e7a6edbbe5367988585df4820801764d1e9
Juan Carlos Sulbaram <sulbaranjc@gmail.com>
2024-04-22 11:09:18 +0200

    PRIMER COMMIT

--- demo/src/main/java/com/example/demo/Controller/ControllerCliente.java
+++ demo/src/main/java/com/example/demo/Controller/ControllerCliente.java
@@ -1,9 +1,16 @@
 package com.example.demo.Controller;
 
+import com.example.demo.entity.Cliente;
 import com.example.demo.service.GestorClientes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Controller
 public class ControllerCliente {
@@ -17,6 +24,26 @@ public class ControllerCliente {
         } catch (Exception ex) {
             return "./error";
         }
-
+    }
+    @GetMapping("/cliente/alta")
+    public String greetingForm(Model model) {
+        model.addAttribute("cliente", new Cliente());
+        return "./cliente/altacliente";
+    }
+    @PostMapping("/cliente/alta")
+    public String greetingSubmit(@ModelAttribute Cliente cliente, Model model) {
+        String valorfinal="redirect:/cliente";
+        try {
+            ge.alta(cliente);
+            try {
+                model.addAttribute("clientes", ge.listar());
+            } catch (SQLException ex) {
+                Logger.getLogger(ControllerCliente.class.getName()).log(Level.SEVERE, null, ex);
+                valorfinal="error";
+            }
+        } catch (SQLException ex) {
+            valorfinal="error";
+        }
+        return valorfinal;
     }
 }
